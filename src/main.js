b050bea13556c09bc0264d934a16bdf410a5ab59
Jun Toda <jun.toda.v36@gmail.com>
2026-02-19 11:35:39 +0900

    Google Analytics 4 の組み込み方法を変更

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import App from "./App.vue";
 import router from "./router";
 import * as VueGtag from "vue-gtag";
 
-createApp(App)
-  .use(
-    VueGtag,
-    {
-      config: {
-        id: "G-5KEJP2C142",
-      },
-    },
-    router,
-  )
-  .use(router)
-  .mount("#app");
+console.log("GA4 Initializing with ID:", "G-XXXXXXXXXX");
+
+const app = createApp(App);
+app.use(router);
+
+// 画面遷移を監視して GA4 に通知
+router.afterEach((to) => {
+  if (window.gtag) {
+    window.gtag("config", "G-XXXXXXXXXX", {
+      page_path: to.fullPath,
+      page_title: to.name || document.title,
+    });
+    console.log("GA4 Tracked:", to.fullPath);
+  }
+});
+
+app.mount("#app");
