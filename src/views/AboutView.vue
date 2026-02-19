1d1326bcb5c02d78f6b67df6b4718b502958b96d
Jun Toda <jun.toda.v36@gmail.com>
2026-02-19 10:55:38 +0900

    Add Vue Router and sample pages

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>ℹ️ About ページ</h2>
+    <p>無事に画面遷移ができました！</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>🏠 ホーム画面</h2>
+    <p>Vue Router を使った遷移のテストです。</p>
+  </div>
+</template>
