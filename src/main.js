82546b9d535022b009914e8326bfb9467c01bcaf
Jun Toda <jun.toda.v36@gmail.com>
2026-02-19 11:23:39 +0900

    Google Analytics 4 を追加

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import * as VueGtag from "vue-gtag";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(
+    VueGtag,
+    {
+      config: {
+        id: "G-5KEJP2C142",
+      },
+    },
+    router,
+  )
+  .use(router)
+  .mount("#app");
