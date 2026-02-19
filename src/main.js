0e988fedf075c385e7bafdfaabd303734ce4761b
Jun Toda <jun.toda.v36@gmail.com>
2026-02-19 13:16:51 +0900

    Google Analytics の ID の誤りを修正

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import * as VueGtag from "vue-gtag";
-
-console.log("GA4 Initializing with ID:", "G-XXXXXXXXXX");
 
 const app = createApp(App);
 app.use(router);
@@ -12,7 +9,7 @@ app.use(router);
 // 画面遷移を監視して GA4 に通知
 router.afterEach((to) => {
   if (window.gtag) {
-    window.gtag("config", "G-XXXXXXXXXX", {
+    window.gtag("config", "G-5KEJP2C142", {
       page_path: to.fullPath,
       page_title: to.name || document.title,
     });
